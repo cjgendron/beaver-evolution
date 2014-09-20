cf7ca50aebdc565bcadf8fc660a5a92b13930f34
Kevin Wang <krwang@mit.edu>
2014-09-20 12:56:38 -0400

    sasdf

--- assets/src/disaster.js
+++ assets/src/disaster.js
@@ -1,8 +1,7 @@
-// var disasters = {Poacher: 0.05, Pollution: 0.1, Lumberjack: 0.15, Forest Fire: 0.2, 
-				 // Flash Flood: 0.2, Drought: 0.15, Famine: 0.1, Tornado: 0.05};
+function disaster_screen(game) {
+	this.game = game;
+	this.disasters = {Poacher: 0.05, Pollution: 0.1, Lumberjack: 0.15, Forest Fire: 0.2, Flash Flood: 0.2, Drought: 0.15, Famine: 0.1, Tornado: 0.05};
 
-var disaster_screen = function(game) {
-	var disasterScreen = document.getElementById("disaster_screen").style.display = "block";
 };
 
 disaster_screen.prototype = {
