703a3295780cfa528e93a48f4a3840d88620ce3d
Justin Martinez <jlmart88@mit.edu>
2014-09-20 13:49:27 -0400

    Board functionally switches between dams/no dams, randomly generates water/land tiles

--- assets/src/board.js
+++ assets/src/board.js
@@ -6,21 +6,6 @@ function Board(game) {
 	this.pieceHeight = this.game.height/(1 + .75 * (this.numPiecesVert - 1));
 	this.pieceWidth = Math.sqrt(3)/2.0 * this.pieceHeight;
 	this.numPiecesHor = Math.floor((this.game.width/this.pieceWidth) - 0.5);
-	console.log(this.pieceWidth);
-	createBoard(this);
-	this.drawBoard(this.game);
-
-	function createBoard(board) {
-		var rowStart = 0;
-		for (var y = 0; y < board.numPiecesVert; y++){
-			for(var x = rowStart; x < rowStart + board.numPiecesHor; x++){
-				board.pieces[[x,y]] = new Piece ("type", board, new HexCoordinate(x, y, board.pieceWidth, board.pieceHeight));
-			}
-			if (y % 2 == 1) {
-				rowStart -= 1;
-			}
-		}
-	};
 };
 
 Board.prototype = {
@@ -40,7 +25,7 @@ Board.prototype = {
 			var rowStart = 0;
 			for (var y = 0; y < board.numPiecesVert; y++){
 				for(var x = rowStart; x < rowStart + board.numPiecesHor; x++){
-					board.pieces[[x,y]] = new Piece ("type", board.game, new HexCoordinate(x, y, board.pieceWidth, board.pieceHeight), board.pieceHeight, board.pieceWidth);
+					board.pieces[[x,y]] = new Piece ("type", board, new HexCoordinate(x, y, board.pieceWidth, board.pieceHeight), board.pieceHeight, board.pieceWidth);
 				}
 				if (y % 2 == 1) {
 					rowStart -= 1;
@@ -49,14 +34,14 @@ Board.prototype = {
 		};
     },
 	placeDam : function(piece){
-		piece.dam = true;
+		//piece.dam = true;
 		if(this.dams.indexOf(piece) == -1) {
 			this.dams.push(piece);
 		}
 	},
 
 	removeDam : function(piece){
-		piece.dam = false;
+		//piece.dam = false;
 		this.dams.splice(this.dams.indexOf(piece), 1);
 	},
 
@@ -78,27 +63,35 @@ function Piece(type, board, hexCoordinate) {
 	this.width = hexCoordinate.width;
 	this.hexCoordinate = hexCoordinate;
 	this.dam = false;
+	this.button;
 
-	this.drawPiece(board);
+	this.drawPiece();
 
 };
 
 Piece.prototype = {
 
-	drawPiece : function(board) {
+	drawPiece : function() {
 		
-		var horizontalOffset = (board.game.width - (board.pieceWidth * (board.numPiecesHor + 0.5))) / 2.0;
-		this.button = this.game.add.button(this.hexCoordinate.pixelCenter['x'] + horizontalOffset, this.hexCoordinate.pixelCenter['y'], "basic_hex_sprite_water", actionOnclick, this);
+		if (this.button != null) {
+			this.button.destroy();
+		}
+		console.log('here');
+		var horizontalOffset = (this.game.width - (this.width * (this.game.getBoard().numPiecesHor + 0.5))) / 2.0;
+		this.button = this.game.add.button(this.hexCoordinate.pixelCenter['x'] + horizontalOffset, 
+			this.hexCoordinate.pixelCenter['y'], 
+			this.dam ? "basic_hex_sprite_dam": 
+				(Math.random() < 0.5 ? "basic_hex_sprite_water" : "basic_hex_sprite_land"), 
+			actionOnClick, this);
 		this.button.scale.x = this.width/this.button.width;
 		this.button.scale.y = this.height/this.button.height;
 		this.button.x -= this.button.width/2;
 		this.button.y -= this.button.height/2;
 
-		function actionOnclick(clickedButton) {
-		   this.dam = true;
-		   this.game.getBoard().placeDam(this);
-		   this.button.destroy();
-		   this.button = this.game.add.button(this.hexCoordinate.pixelCenter['x'] - this.width/2, this.hexCoordinate.pixelCenter['y'] - this.height/2, "green_brown", actionOnclick, this);
+		function actionOnClick(clickedButton) {
+		   this.dam = !this.dam;
+		   this.dam ? this.game.getBoard().placeDam(this) : this.game.getBoard().removeDam(this);
+		   this.drawPiece();
 		}
 	},
 
