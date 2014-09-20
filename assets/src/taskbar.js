423be7fd352c2a472c6e41f83a5ccd36d6450dc8
kevinwen <kevinwen94@gmail.com>
2014-09-20 13:40:55 -0400

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	index.html

--- assets/src/taskbar.js
+++ assets/src/taskbar.js
@@ -1,20 +1,28 @@
 function Taskbar(game) {
 	this.game = game;
+	board = game.getBoard()
+	this.damCount = board.getDamCount();
 	createTaskbar(this);
 
 	function createTaskbar(taskbar){
-		buildButton = game.add.button(300, 500, 'build', actionOnBuild, this);
-		populateButton = game.add.button(400, 500, 'populate', actionOnPopulate, this);
-		evolveButton = game.add.button(500, 500, 'evolve', actionOnEvolve, this);
+		taskbar.beaverImage = game.add.image(100, 500, 'beaverImage');
+		taskbar.beaverCountText = game.add.text(150, 500, 'numBeavers', {fill: "#ff0044"});
+		taskbar.damImage = game.add.image(200, 500, 'damImage');
+		taskbar.damCountText = game.add.text(250, 500, taskbar.damCount, { fill: "#ff0044"});
+		taskbar.buildButton = game.add.button(500, 500, 'build', actionOnBuild, taskbar);
+		taskbar.populateButton = game.add.button(600, 500, 'populate', actionOnPopulate, taskbar);
+		taskbar.evolveButton = game.add.button(700, 500, 'evolve', actionOnEvolve, taskbar);
 	}
 
-	function actionOnBuild(){
-		// damCount = board.getDamCount;
-		// damCount += Math.ceil(0.5 * board.numBeavers);
+	function actionOnBuild(taskbar){
+		this.damCount += 1;
+		console.log(this.damCount);
+		this.damCountText.setText(this.damCount);
 	}
 
 	function actionOnPopulate(){
 		// numBeavers = Math.ceil(0.25 * board.numBeavers);
+		// beaverCountText.setText(numBeavers);
 	}
 
 	function actionOnEvolve(){
