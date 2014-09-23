309fa9cc276567f61df0bda09b523f98a178cefb
Jordan Haines <jordan@kalosal.com>
2014-09-23 16:16:38 -0400

    Start, End stages (game now starts with home screen)

---
+++ assets/src/endStage.js
@@ -0,0 +1,36 @@
+var EndStage = function (game) {
+	this.instructionText = null;
+	this.replayButton = null;
+}
+
+EndStage.prototype = {
+
+	init: function () {
+		this.input.maxPointers = 1;
+		this.stage.disableVisibilityChange = true;
+	},
+
+	preload: function() {
+		this.load.image('homeBackground', 'assets/images/background.png');
+		this.load.image('replayButton', 'assets/images/replay_button.png');
+
+	},
+
+	create: function() {
+		this.add.sprite(0, 0, 'homeBackground');
+
+		// Add some instructions
+		this.instructionText = this.add.text(140, 130, "Game Over");
+		this.instructionText.fontSize = 110;
+		this.instructionText.fill = "#FFFFFF";
+		this.instructionText.wordWrapWidth = 600;
+
+		this.replayButton = this.add.button(200, 350, 'replayButton', this.replayGame);
+	},
+
+	replayGame: function() {
+		// Is this enough to restart the game - we'll see!
+		this.game.state.start('main');
+
+	}
+}
---
+++ assets/src/homeStage.js
@@ -0,0 +1,53 @@
+var HomeStage = function (game) {
+	this.instructionText = null;
+	this.playButton = null;
+	this.game = game;
+
+	this.instructionText = [
+		"Choose to evolve your colony to better withstand disasters over time",
+		"Every turn, you will have the option to add more beavers or more dams",
+		"Each turn also brings the chance of a random natural disaster, which can devastate your colony",
+		"Evolve your colony of beavers over time to withstand natural disasters!",
+	];
+}
+
+HomeStage.prototype = {
+
+	init: function () {
+		this.input.maxPointers = 1;
+		this.stage.disableVisibilityChange = true;
+	},
+
+	preload: function() {
+		this.load.image('homeBackground', 'assets/images/background.png');
+		this.load.image('playButton', 'assets/images/play_button.png');
+		this.load.image('homeBeaver1', 'assets/images/beaver.png');
+		this.load.image('homeBeaver2', 'assets/images/beaver2.png');
+
+	},
+
+	create: function() {
+		this.add.sprite(0, 0, 'homeBackground');
+		// Title
+		this.title = this.add.text(120, 40, "Beaver Evolution");
+		this.title.fontSize = 80;
+		this.title.fill = "#FFFFFF";
+
+		// Add some instructions
+		var currentY = 150;
+		var spacing = 35;
+		for (var i = 0; i < this.instructionText.length; i++) {
+			newText = this.add.text(20, currentY, this.instructionText[i]);
+			newText.fontSize = 20;
+			this.instructionText[i].wordWrapWidth = 600;
+			currentY += spacing;
+		};
+		this.playButton = this.add.button(520, 430, 'playButton', this.startGame);
+		this.beaver2 = this.add.sprite(30, 310, 'homeBeaver2');
+	},
+
+	startGame: function() {
+		// User clicks 'start game' - let's get going
+		this.game.state.start('main');
+	}
+}
